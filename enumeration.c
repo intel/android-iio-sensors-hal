67575b96002694d57b1c22888f84bc016997237a
Patrick Porlan <patrick.porlan@intel.com>
2014-04-14 17:36:23 +0200

    STPK-1429 Correct Android type for ALS sensor
    Thanks to Goran Roth for pointing out that TYPE_TEMPERATURE was
    deprecated in favor to TYPE_AMBIENT_TEMPERATURE in API level 14.
    
    Issue: STPK-1429
    
    Change-Id: I4d67adaeda6e4b4f68a53a347d838770863ce923
    Signed-off-by: Patrick Porlan <patrick.porlan@intel.com>

--- enumeration.c
+++ enumeration.c
@@ -24,7 +24,7 @@ struct sensor_catalog_entry_t sensor_catalog[] = {
 	DECLARE_SENSOR3("incli",      SENSOR_TYPE_ORIENTATION,    "x", "y", "z")
 	DECLARE_SENSOR4("rot",        SENSOR_TYPE_ROTATION_VECTOR,
 					 "quat_x", "quat_y", "quat_z", "quat_w")
-	DECLARE_SENSOR0("temp",	      SENSOR_TYPE_TEMPERATURE 		       )
+	DECLARE_SENSOR0("temp",	      SENSOR_TYPE_AMBIENT_TEMPERATURE	       )
 };
 
 #define CATALOG_SIZE	ARRAY_SIZE(sensor_catalog)
