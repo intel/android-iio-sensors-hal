d52af6c327c742241df3b682c80d7fb4327b173b
Octavian Purdila <octavian.purdila@intel.com>
2014-08-11 14:46:19 +0300

    sens: update properties name and value max size
    Update the Linux properties name and value max size to match those in
    Android.
    
    Change-Id: I9ac24242b1056a5e65f921a31d9ec583d166bae2
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

--- linux/cutils/properties.h
+++ linux/cutils/properties.h
@@ -5,8 +5,8 @@
 #ifndef _LINUX_APROP_H
 #define _LINUX_APROP_H
 
-#define PROP_VALUE_MAX 16
-#define PROP_NAME_MAX 16
+#define PROP_VALUE_MAX 92
+#define PROP_NAME_MAX 32
 
 static inline int property_get(const char *name, const char *val, const char *x)
 {
