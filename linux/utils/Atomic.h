9069fc7f4119f7f64e0339fdec22dc952a7e14c6
Octavian Purdila <octavian.purdila@intel.com>
2014-11-20 14:26:29 +0200

    Fix Linux build
    Add Atomic.h and android_alarm.h stubs.
    
    Change-Id: I5e044de52ab7ab5f8b68e3361f327adb61b5f3a0
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

---
+++ linux/utils/Atomic.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2014 Intel Corporation.
+ */
+
+#ifndef _LINUX_ATOMIC_H
+#define _LINUX_ATOMIC_H
+
+int android_atomic_release_cas(int32_t oldvalue, int32_t newvalue,
+			       volatile int32_t* addr)
+{
+	return !__sync_bool_compare_and_swap(addr, oldvalue, newvalue);
+}
+
+#define android_atomic_cmpxchg android_atomic_release_cas
+
+
+#endif /* _LINUX_ATOMIC_H */
