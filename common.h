89f2b9b7744f165a604e88fb9d7c805abb5924d5
Patrick Porlan <patrick.porlan@intel.com>
2015-02-13 16:08:58 +0100

    Fix type of recently introduced semi arbitrated sampling rate
    Sammpling rates are not necessarily integers. Sure, they are in
    the case of the MPU-6050, which is fortunately the only chip we
    support at the moment with several interrupt driven sensors on
    the same iio device, so this is not an issue for now, but still.
    
    Change-Id: Id3fb9a9fb2c6fc2ae032f6484088f14a99e75670
    Signed-off-by: Patrick Porlan <patrick.porlan@intel.com>

--- common.h
+++ common.h
@@ -375,7 +375,7 @@ typedef struct
 	 */
 	int needs_enable;
 
-	int semi_arbitrated_rate;	/* Arbitrated sampling rate before we considered other sensors co-located on the same iio device */
+	float semi_arbitrated_rate;	/* Arbitrated sampling rate before we considered other sensors co-located on the same iio device */
 }
 sensor_info_t;
 
